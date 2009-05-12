b1eb2eaa6f5621124b544bd8590f12095035c09c
sofia.jonsson <sofia.jonsson@callistaenterprise.se>
2009-05-12 17:19:25 +0000

    Image serving servlet

---
+++ core-bc/modules/web/src/main/java/se/vgr/webbisar/web/ImageServlet.java
@@ -0,0 +1,121 @@
+package se.vgr.webbisar.web;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URLDecoder;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import se.vgr.webbisar.svc.Configuration;
+
+/**
+ * Servlet implementation class ImageServlet
+ */
+public class ImageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+     
+    private static final int DEFAULT_BUFFER_SIZE = 10240; // 10KB.
+
+  
+    private String imagePath;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ImageServlet() {
+        super();
+    }
+
+	
+
+	@Override
+	public void init() throws ServletException {
+		WebApplicationContext ctx  = WebApplicationContextUtils.getWebApplicationContext(getServletContext());
+	    Configuration config = (Configuration)ctx.getBean("configuration");  
+	    imagePath = config.getImageBaseDir();
+		super.init();
+	}
+
+
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException
+    {
+        // Get requested image by path info.
+        String requestedImage = request.getPathInfo();
+
+        if (requestedImage == null) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND); // 404.
+            return;
+        }
+
+        File image = new File(imagePath, URLDecoder.decode(requestedImage, "UTF-8"));
+
+        // Check if file exists 
+        if (!image.exists()) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND); // 404.
+            return;
+        }
+
+        // Get content type by filename.
+        String contentType = getServletContext().getMimeType(image.getName());
+
+        if (contentType == null || !contentType.startsWith("image")) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND); // 404.
+            return;
+        }
+
+        // Init servlet response.
+        response.reset();
+        response.setBufferSize(DEFAULT_BUFFER_SIZE);
+        response.setHeader("Content-Type", contentType);
+        response.setHeader("Content-Length", String.valueOf(image.length()));
+        response.setHeader("Content-Disposition", "inline; filename=\"" + image.getName() + "\"");
+
+        // Prepare streams.
+        BufferedInputStream input = null;
+        BufferedOutputStream output = null;
+
+        try {
+            // Open streams.
+            input = new BufferedInputStream(new FileInputStream(image), DEFAULT_BUFFER_SIZE);
+            output = new BufferedOutputStream(response.getOutputStream(), DEFAULT_BUFFER_SIZE);
+
+            // Write file contents to response.
+            byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+            int length;
+            while ((length = input.read(buffer)) > 0) {
+                output.write(buffer, 0, length);
+            }
+
+            // Finalize task.
+            output.flush();
+        } finally {
+            // Gently close streams.
+            close(output);
+            close(input);
+        }
+    }
+
+ 
+    private static void close(Closeable resource) {
+        if (resource != null) {
+            try {
+                resource.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
