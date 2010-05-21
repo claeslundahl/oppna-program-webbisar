13337331190cc41757bc92f781d3c3f1304447c7
asse.hemma <asse.hemma@b06c261a-3169-11de-96ab-f16d9912eac9>
2010-05-21 11:43:50 +0000

    Back to start page did not work properly in IE

--- trunk/core-bc/modules/web/src/main/java/se/vgr/webbisar/web/ErrorServlet.java
+++ trunk/core-bc/modules/web/src/main/java/se/vgr/webbisar/web/ErrorServlet.java
@@ -18,8 +18,8 @@
 package se.vgr.webbisar.web;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -31,10 +31,10 @@ import org.apache.log4j.Logger;
  * Servlet implementation class ErrorServlet
  */
 public class ErrorServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-	static final Logger log = Logger.getLogger(ErrorServlet.class);
-	
+    private static final long serialVersionUID = 1L;
+
+    static final Logger log = Logger.getLogger(ErrorServlet.class);
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -42,35 +42,39 @@ public class ErrorServlet extends HttpServlet {
         super();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession();
-		Errors errors = (Errors) session.getAttribute("internalErrors");
-		if(errors == null) {
-			log.debug("Received first error");
-			errors = new Errors(10000);
-			session.setAttribute("internalErrors", errors);
-		}
-		
-		errors.addError();
-		log.debug("Number of errors " + errors.getNumOfErrors());
-		if(errors.getNumOfErrors() > 3) {
-			log.debug("Number of errors exceeded, redirect to " + request.getContextPath() + "/error.jsp");
-			session.invalidate();
-			response.sendRedirect(request.getContextPath() + "/error.jsp");			
-		} else { 
-			log.debug("Redirect to " + request.getContextPath() + "/index.jsp");
-			response.sendRedirect(request.getContextPath() + "/index.jsp");
-		}
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+        HttpSession session = request.getSession();
+        Errors errors = (Errors) session.getAttribute("internalErrors");
+        if (errors == null) {
+            log.debug("Received first error");
+            errors = new Errors(10000);
+            session.setAttribute("internalErrors", errors);
+        }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-	}
+        errors.addError();
+        log.debug("Number of errors " + errors.getNumOfErrors());
+        if (errors.getNumOfErrors() > 3) {
+            log.debug("Number of errors exceeded, redirect to " + request.getContextPath() + "/error.jsp");
+            session.invalidate();
+            response.sendRedirect(request.getContextPath() + "/error.jsp");
+        } else {
+            log.debug("Redirect to " + request.getContextPath() + "/index.jsp");
+            response.sendRedirect(request.getContextPath() + "/index.jsp");
+        }
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+        doGet(request, response);
+    }
 
 }
