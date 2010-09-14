f4d7c3dafb7f196503fd79823e0d3e8254e033f9
asse.hemma <asse.hemma@b06c261a-3169-11de-96ab-f16d9912eac9>
2010-09-14 13:06:40 +0000

    Now using release version of se.vgregion.commons-util

--- trunk/core-bc/modules/web/src/main/java/se/vgregion/webbisar/web/WebbisExtraInfoSitemapServlet.java
+++ trunk/core-bc/modules/web/src/main/java/se/vgregion/webbisar/web/WebbisExtraInfoSitemapServlet.java
@@ -19,71 +19,26 @@
 
 package se.vgregion.webbisar.web;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import se.vgregion.sitemap.service.SitemapService;
+import se.vgregion.sitemap.servlet.DefaultSitemapServlet;
 
 /**
- * Generates an internal sitemap for Webbisar.
+ * Generates an ExtraInfo (internal) sitemap for Webbisar.
  */
-public class WebbisExtraInfoSitemapServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-    private static final String ENCODING_UTF8 = "UTF-8";
-    private static final String CLASS_NAME = WebbisExtraInfoSitemapServlet.class.getName();
-    private static final Log LOGGER = LogFactory.getLog(WebbisExtraInfoSitemapServlet.class);
-
-    private SitemapService<?> sitemapService;
+public class WebbisExtraInfoSitemapServlet extends DefaultSitemapServlet {
+    private static final long serialVersionUID = 4782818023575166386L;
 
     /**
-     * Get reference to SitemapSupportBean from Spring context.
-     * 
-     * @throws ServletException
-     *             if an exception occurs that interrupts the servlet's normal operation.
+     * {@inheritDoc}
      */
     @Override
-    public void init() throws ServletException {
-        LOGGER.info(CLASS_NAME + ".init()");
-        super.init();
-
+    protected void loadSitemapService() {
         WebApplicationContext springContext = WebApplicationContextUtils
                 .getWebApplicationContext(getServletContext());
 
-        sitemapService = (SitemapService<?>) springContext.getBean("internalSitemapService");
-    }
-
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        LOGGER.debug(CLASS_NAME + ".doGet()");
-        LOGGER.debug("WebbisExtraInfoSitemapServlet starting to put together the sitemap.");
-
-        long startTimeMillis = System.currentTimeMillis();
-
-        String sitemapContent = sitemapService.getSitemapContent();
-
-        long endTimeMillis = System.currentTimeMillis();
-
-        LOGGER.debug("WebbisExtraInfoSitemapServlet generation finished. It took: "
-                + (endTimeMillis - startTimeMillis) / 1000 + " seconds.");
-
-        response.setCharacterEncoding(ENCODING_UTF8);
-
-        PrintWriter pw = response.getWriter();
-        pw.write(sitemapContent);
-        pw.flush();
-        pw.close();
+        setSitemapService((SitemapService<?>) springContext.getBean("internalSitemapService"));
     }
 }
